3319125651235dcb4060de6c2eee6f31a4b50995
Masudur Rahman <115950428+masudpatwary@users.noreply.github.com>
2023-12-31 11:47:44 +0600

    update

--- js/meals.js
+++ js/meals.js
@@ -11,6 +11,7 @@ const loadDataDisplay = (meals) => {
   meals.forEach((meal) => {
     // console.log(meal);
     const newDataDiv = document.createElement("div");
+    newDataDiv.classList.add("item-design");
 
     newDataDiv.innerHTML = `
 <a href="#">
@@ -23,12 +24,9 @@ const loadDataDisplay = (meals) => {
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-600">Here are the biggest enterprise technology acquisitions of 2021 so
         far, in
         reverse chronological order.</p>
-    <a href="#"
-        class="inline-flex items-center py-2 text-sm font-bold text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 px-8">
-        Order
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-        </svg>
-    </a>
+    <button onclick="orderMeals(${meal.idMeal})" type="button" class="btn btn-primary py-2 px-8 bg-blue-500" data-bs-toggle="modal" data-bs-target="#mealsDetails">
+    Order
+</button>
 </div>
   `;
     innerData.appendChild(newDataDiv);
@@ -38,6 +36,22 @@ const loadDataDisplay = (meals) => {
 const inputField = () => {
   const inputField = document.getElementById("input-field").value;
   loadMials(inputField);
+};
+
+const orderMeals = (orderItem) => {
+  console.log(orderItem);
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${orderItem}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => confirmOrderDetails(data.meals[0]));
+};
+const confirmOrderDetails = (meal) => {
+  const title = document.getElementById("mealsDetailsLabel");
+  title.innerText = meal.strMeal;
+
+  const body = document.getElementById("modal-body");
+  body.innerHTML = `<p class="p-2">This is supper food for our gellary of mealBD (${meal.strCategory})</p>`;
 };
 
 loadMials("a");
